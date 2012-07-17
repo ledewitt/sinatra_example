bdc4d03d6506706d35e19b2b51a108df75e714d0
Luke DeWitt <ledewitt@gmail.com>
2012-07-17 14:07:28 -0600

    Some functionality

--- luke_wars.rb
+++ luke_wars.rb
@@ -45,9 +45,30 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
-SECTORS = { }
+# SECTORS = { 2000 3401 1111 3219 }
+
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params[:name] is 'foo' or 'bar'
+  "Hello #{params[:name]}!"
+end
 
 get("/sector/:number") {
   erb :sector
   # "Number: #{params[:number]}"
 }
+
+get '/say/*/to/*' do
+  # matches /say/hello/to/world
+  params[:splat] # => ["hello", "world"]
+end
+
+set(:probability) { |value| condition { rand <= value } }
+
+get '/win_a_car', :probability => 0.5 do
+  "You won!"
+end
+
+get '/win_a_car' do
+  "Sorry, you lost."
+end
